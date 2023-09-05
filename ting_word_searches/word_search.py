b9d234b22dfb88bccd0f40e7679b72b89c9f5c30
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-09-05 06:16:09 -0300

    REQ 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list = []
+    lower = word.lower()
+
+    for length in range(len(instance)):
+        file = instance.search(length)
+        info = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, file_line in enumerate(file["linhas_do_arquivo"])
+                if lower in file_line.lower()
+            ],
+        }
+
+        if info["ocorrencias"]:
+            list.append(info)
+
+    return list
 
 
 def search_by_word(word, instance):
