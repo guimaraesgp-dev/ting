2010416a28df67056748036d2bbb04961a80f700
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-09-05 05:24:54 -0300

    REQ 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list = []
+    line = txt_importer(path_file)
+
+    for length in range(len(instance)):
+        if not instance.search(length)["nome_do_arquivo"]:
+            return None
+        list.append(instance.search(length)["nome_do_arquivo"])
+
+    if path_file not in list:
+        file_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(line),
+            "linhas_do_arquivo": line,
+        }
+        instance.enqueue(file_info)
+        sys.stdout.write(str(file_info))
 
 
 def remove(instance):
